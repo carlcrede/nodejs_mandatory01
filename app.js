fc463f2ef4b6c3e5a668ead374b798bb2d74d2aa
Christian <carl_hassel@hotmail.com>
2021-03-17 19:37:44 +0100

    App created, first steps done

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/js', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html');
+});
+
+app.get('/node', (req, res) => {
+    res.sendFile(__dirname + '/public/node.html');
+});
+
+app.get('/express', (req, res) => {
+    res.sendFile(__dirname + '/public/express.html');
+});
+
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`Server running on port ${PORT}`);
+    }
+});
